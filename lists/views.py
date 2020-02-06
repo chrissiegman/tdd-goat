993291b5ec4bfe85fd456280a77abc04631c9806
Chris Siegman <csiegman@ok7-eleven.com>
2020-02-06 16:21:29 -0600

    Speed up functional tests

--- lists/views.py
+++ lists/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 from lists.models import Item
 
-# Create your views here.
+
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
