291865fce19b611d936af1f71aafad631591e147
Chris Siegman <csiegman@ok7-eleven.com>
2020-02-10 09:32:19 -0600

    Add foreign key relationship to Item model

--- lists/models.py
+++ lists/models.py
@@ -7,6 +7,6 @@ class List(models.Model):
 
 class Item(models.Model):
     text = models.TextField(default='')
-    list = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
 
 
--- lists/tests.py
+++ lists/tests.py
@@ -23,8 +23,6 @@ class ListAndItemModelTest(TestCase):
         list_ = List()
         list_.save()
 
-
-
         first_item = Item()
         first_item.text = 'The first (ever) list item'
         first_item.list = list_
@@ -52,8 +50,9 @@ class ListAndItemModelTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
